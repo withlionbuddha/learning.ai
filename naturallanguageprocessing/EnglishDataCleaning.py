dc97148eb6fd3aab62beb4cd4255407e9f0b8f03
WithLion <136729489+withlionbuddha@users.noreply.github.com>
2024-10-01 20:59:27 +0900

    자연어 처리중 전처리 단계 datacleaning

---
+++ naturallanguageprocessing/EnglishDataCleaning.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import re
+from DataCleaning import DataCleaning
+
+class EnglishDataCleaning(DataCleaning):
+    def remove_special_characters(self):
+        # 영어 특수 문자 및 숫자 제거
+        cleaned_text = re.sub(r"[^a-zA-Z\s]", "", self.text)
+        return cleaned_text
+
+    def remove_html_tags(self):
+        # HTML 태그 제거
+        cleaned_text = re.sub(r"<[^>]+>", "", self.text)
+        return cleaned_text
+
+    def to_lowercase(self):
+        # 영어는 대소문자 통일
+        return self.text.lower()
+
+
+# In[ ]:
+
+
+
+
---
+++ naturallanguageprocessing/KoreanDataCleaning.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import re
+from DataCleaning import DataCleaning
+
+class KoreanDataCleaning(DataCleaning):
+    def remove_special_characters(self):
+        # 한국어 특수 문자 및 숫자 제거
+        cleaned_text = re.sub(r"[^가-힣\s]", "", self.text)
+        return cleaned_text
+
+    def remove_html_tags(self):
+        # HTML 태그 제거
+        cleaned_text = re.sub(r"<[^>]+>", "", self.text)
+        return cleaned_text
+
+    def to_lowercase(self):
+        # 한국어는 대소문자 구분 없음, 그대로 반환
+        return self.text
+
+
+# In[ ]:
+
+
+
+
