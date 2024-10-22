9d35d7f1b8ef9a9b5abbd441e30fa5bbdd23c6ee
WithLion <136729489+withlionbuddha@users.noreply.github.com>
2024-10-22 20:45:25 +0900

    static activation function

--- fullyconnectneuralnetwork/ActivationFunction.py
+++ fullyconnectneuralnetwork/ActivationFunction.py
@@ -5,6 +5,7 @@ class ReLU:
     @staticmethod
     def activation_fn(z: np.ndarray) -> np.ndarray:
         print("activation function is relu")
+
         return np.maximum(0, z)
     
     @staticmethod
@@ -24,9 +25,16 @@ class Sigmoid:
 
 class Softmax:
     
+    ''' softmax(self, z: np.ndarray) -> np.ndarray
+        출력값이 범위 0 <= softmax(z) <= 1
+        Softmax(z_i) = e^(z_i - z_max) / ∑(j=1 to n) e^(z_j - z_max)
+    '''
+    
     @staticmethod
     def activation_fn(z: np.ndarray) -> np.ndarray:
         print("activation function is softmax")
+        if z is None or np.any(z == None):
+            raise ValueError("Invalid input: None values detected in softmax input.")
         
         z_max = np.max(z, axis=-1, keepdims=True)
         exp_for_z = np.exp(z - z_max)
--- fullyconnectneuralnetwork/MultiInputNeuron.py
+++ fullyconnectneuralnetwork/MultiInputNeuron.py
@@ -19,35 +19,4 @@ class MultiInputNeuron:
         
         print(f"[neuron.update] weights {weights}, bias {bias}")
     
-    ''' softmax(self, z: np.ndarray) -> np.ndarray
-        출력값이 범위 0 <= softmax(z) <= 1
-        Softmax(z_i) = e^(z_i - z_max) / ∑(j=1 to n) e^(z_j - z_max)
-    '''
-    @staticmethod
-    def softmax(z: np.ndarray) -> np.ndarray:
-        print("activation function is softmax")
-        if z is None or np.any(z == None):
-            raise ValueError("Invalid input: None values detected in softmax input.")
-        
-        z_max = np.max(z, axis=-1, keepdims=True)
-        exp_for_z = np.exp(z - z_max) 
-        sum_exp_for_z = np.sum(exp_for_z, axis=-1, keepdims=True)  
-        return exp_for_z / sum_exp_for_z
-
-    @staticmethod
-    def sigmoid(z: np.ndarray) -> np.ndarray:
-        return 1 / (1 + np.exp(-z))
-    
-    @staticmethod
-    def sigmoid_derivative(z: np.ndarray) -> np.ndarray:
-        return z * (1 - z)
-
-    @staticmethod
-    def relu(z: np.ndarray) -> np.ndarray:
-        print("activation function is relu")
-        return np.maximum(0, z)
-    
-    @staticmethod
-    def relu_partial_derivative(z: np.ndarray) -> np.ndarray :
-        print("relu partial derivative")
-        return 0
+  
